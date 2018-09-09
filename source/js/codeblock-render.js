ddc668c1f285ba2a586eb8755d2ce941a7c17e6c
Kami <mail@kamiboy.me>
2018-09-10 00:14:40 +0800

    update

---
+++ source/js/codeblock-render.js
@@ -0,0 +1,6 @@
++function($) {
+    $('pre').addClass('prettyprint');
+    prettyPrint();
+    $('.prettyprint').css({'border': '0px', 'font-style': 'italic'});
+    $('.com').removeClass('com');
+}(jQuery);
